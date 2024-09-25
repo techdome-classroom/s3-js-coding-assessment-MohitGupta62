4d1b8b3d58f8d67aa82a2042549f16fd532fa417
Mohit Gupta <chintu3122944@gmail.com>
2024-09-25 15:31:35 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,10 +2,11 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+var isValid = function (s) {
+  const matchingBracket = {
+    ")": "(",
+    "}": "{",
+  };
 };
 
 module.exports = { isValid };
-
-

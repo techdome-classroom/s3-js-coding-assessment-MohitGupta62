497153b7cf4778fcff0dbd592142628812f7eb44
Mohit Gupta <chintu3122944@gmail.com>
2024-09-25 15:34:15 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -13,7 +13,7 @@ var isValid = function (s) {
     if (char in matchingBracket) {
       const topElement = stack.length > 0 ? stack.pop() : "#";
 
-      if (matchingBracket[char]) {
+      if (matchingBracket[char] !== topElement) {
         
       }
     }

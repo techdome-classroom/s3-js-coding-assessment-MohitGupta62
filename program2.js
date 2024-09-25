3fe7f7909e3ba3151de8a2ad2241d4a3e692e7ee
Mohit Gupta <chintu3122944@gmail.com>
2024-09-25 16:04:13 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -16,7 +16,7 @@ const romanMap ={
     let prevValue = 0;
     
     for(let i = s.length -1; i>=0; i-- ){
-        const currentValue = romanMap[s[i]]
+        const currentValue = romanMap[s[i]];
     }
 };
 
